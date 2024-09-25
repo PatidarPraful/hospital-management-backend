7e10705dbe2fe380ad5e3672b0cb8d6f6aaaf2b9
PatidarPraful <114163014+PatidarPraful@users.noreply.github.com>
2024-09-26 01:30:38 +0530

    Update jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -6,6 +6,7 @@ export const generateToken = (user,message,statusCode, res)=>{
         expires : new Date(Date.now() + process.env.COOKIE_EXPIRE *24 * 60 * 60 * 1000),
         httpOnly : true,
         sameSite: "None",
+        secure : true
     }).json({
         success : true,
         message,
